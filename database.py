0ee25463cb3efb0ffe50a20c4656590a6d3509f6
alei5293 <alexleijun@gmail.com>
2024-04-25 17:18:26 +1000

    Drafter SQL Query for part 2

--- database.py
+++ database.py
@@ -10,8 +10,8 @@ Connect to the database using the connection string
 '''
 def openConnection():
     # connection parameters - ENTER YOUR LOGIN AND PASSWORD HERE
-    userid = "y24s1c9120_unikey"
-    passwd = ""
+    userid = "y24s1c9120_alei5293"
+    passwd = "Sunny445."
     myHost = "awsprddbs4836.shared.sydney.edu.au"
 
     # Create a connection to the database
@@ -33,15 +33,46 @@ Validate staff based on username and password
 '''
 def checkStaffLogin(staffID, password):
 
-    return ['johndoe', '654', 'John', 'Doe', 22, 38000]
+    curs = openConnection().cursor()
+    query = f"SELECT * FROM staff WHERE staffID = '{staffID}' and password = '{password}'"
+    curs.execute(query)
+    row = curs.fetchall()
+    curs.close()
+    if row == []:
+        return None
+    return list(row[0])
 
-
+print(checkStaffLogin('jwalker','876'))
 '''
 List all the associated menu items in the database by staff
 '''
 def findMenuItemsByStaff(staffID):
 
     return
+
+'''
+SQL Query for part 2. 
+
+TO DO:
+    - Change WHERE to the staffid 
+    - Concatenate category names to "Breakfast" or "Breakfast|Lunch|Dinner" etc.
+    - Concatenate c.coffeetypename and mk.milkkindname to Option
+    - Concantenate s.firstname, s.lastname to name
+
+SELECT m.menuitemid, m.name, m.description, cat1.categoryname as categoryname1, cat2.categoryname as categoryname2, cat3.categoryname as categoryname3, c.coffeetypename, mk.milkkindname, m.price, m.reviewdate, s.firstname, s.lastname 
+FROM menuitem m LEFT OUTER JOIN category cat1 ON (m.categoryone = cat1.categoryid) 
+				LEFT OUTER JOIN category cat2 ON (m.categorytwo = cat2.categoryid)
+				LEFT OUTER JOIN category cat3 ON (m.categorythree = cat3.categoryid)
+				LEFT OUTER JOIN coffeetype c ON (m.coffeetype = c.coffeetypeid)
+				LEFT OUTER JOIN milkkind mk ON (m.milkkind = mk.milkkindid)
+				LEFT OUTER JOIN staff s ON (m.reviewer = s.staffid)
+WHERE m.reviewer = 'johndoe'
+ORDER BY m.reviewdate, m.description DESC
+
+'''
+
+
+
 
 
 '''
@@ -67,3 +98,10 @@ Update an existing menu item
 def updateMenuItem(name, description, categoryone, categorytwo, categorythree, coffeetype, milkkind, price, reviewdate, reviewer):
 
     return
+
+
+
+
+
+
+
